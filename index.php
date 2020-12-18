3f5b9b8d89d8f6f43646aae95a039d58aeb390a5
akmal-dev <akmalaquila07@gmail.com>
2020-12-18 22:16:44 +0800

    pertama

---
+++ index.php
@@ -0,0 +1,224 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Rumiver</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FreeHTML5.co" />
+
+  <!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FreeHTML5.co
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+	
+	
+	<div id="fh5co-page">
+	<header id="fh5co-header" role="banner">
+		<div class="container">
+			<div class="header-inner">
+				<h1><a href="index.html">Rumiver</a></h1>
+				<nav role="navigation">
+					<ul>
+					
+						<li><a href="about.html">Login</a></li>
+						<li class="cta"><a href="#">Daftar</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	
+	<div class="container">
+		<div class="col-md-5 animate-box">
+			<div class="row">
+				<div class="col-md-10  text-left fh5co-heading animate-box" style="margin-top: 15%;">
+					<h1><b>Solusi mental health online</b></h1>
+					<p>Bebaskan pikiranmu dan redakan semua permasalahanmu disini lebih nyaman dan terpercaya</p>
+				</div>
+				<div class="col-md-12 text-left animate-box">
+					<p><a href="#" class="btn btn-primary with-arrow">Coba Sekarang <i class="icon-arrow-right"></i></a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-7 animate-box">
+			<div class="image"><img src="images/slide3.jpg" width="620vmax" height="500vmax"></div>
+		</div>
+		
+	</div>
+
+	<div id="fh5co-services-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading animate-box">
+					<h2>Fitur Spesial</h2>
+					<p>Rasakan fitur-fitur yang akan membantu kamu untuk menjadi lebih baik lagi </p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 animate-box">
+					<div class="services">
+						<i class="icon-laptop"></i>
+						<div class="desc">
+							<h3>Konsultasi online</h3>
+							<p>Tanya langsung dengan psikolog terpercaya yang siap membantu menyelesaikan permasalahanmu</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 animate-box">
+					<div class="services">
+						<i class="icon-server"></i>
+						<div class="desc">
+							<h3>Mood tracker</h3>
+							<p>Catat mood kamu setiap hari untuk mengetahui apa yang sering terjadi pada dirimu </p>
+						</div>
+					</div>
+				</div>
+				
+				
+				
+				
+			</div>
+		</div>
+	</div>
+	
+	
+	
+	
+	<div id="fh5co-blog-section" class="fh5co-light-grey-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading animate-box">
+					<h2>Artikel Terkini</h2>
+					<p>Baca artikel-artikel terbaru yang membantu kamu lebih termotivasi </p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 col-sm-6 animate-box">
+					<a href="#" class="item-grid">
+						<div class="image" style="background-image: url(images/talking.jpg)"></div>
+						<div class="v-align">
+							<div class="v-align-middle">
+								<h3 class="title">Ternyata Bisa Lebih Lega, Ini Alasan Cerita ke Psikolog Itu Juga Asyik</h3>
+								<h5 class="date"><span>18 Desember, 2020</span></h5>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-6 animate-box">
+					<a href="#" class="item-grid">
+						<div class="image" style="background-image: url(images/happy.jpg)"></div>
+						<div class="v-align">
+							<div class="v-align-middle">
+								<h3 class="title">Gak Kepikiran! 5 Kebiasaan Unik Ini Bisa Bikin Mood Tambah Happy</h3>
+								<h5 class="date"><span>18 Desember, 2020</span></h5>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.</p>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-12 text-center animate-box">
+					<p><a href="#" class="btn btn-primary with-arrow">Lihat Lebih Banyak <i class="icon-arrow-right"></i></a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+
+	<div class="fh5co-cta" style="background-image: url(images/lega.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="col-md-12 text-center animate-box">
+				<h3>Mau rasain fiturnya lebih detail?</h3>
+				<p><a href="#" class="btn btn-primary btn-outline with-arrow">Daftar Sekarang <i class="icon-arrow-right"></i></a></p>
+			</div>
+		</div>
+	</div>
+
+	
+	<footer id="fh5co-footer" role="contentinfo">
+	
+		<div class="container text-center">
+			<p>&copy; Rumiver 2020. All Rights Reserved. <span>Designed with <i class="icon-heart"></i> by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a></span></p>
+			
+			
+		</div>
+	</footer>
+	</div>
+	
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Owl Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+
+	<!-- MAIN JS -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
